84c1ff9d02c7391cb88b6f1a344d1a075b022838
tobidot <Object.name@live.de>
2019-09-08 09:58:15 +0200

    Color Heaven and Hellstone and give it mass

--- Gameplay.cpp
+++ Gameplay.cpp
@@ -10,6 +10,10 @@ namespace gameplay {
 			return olc::Pixel(0xff446688);
 		case AIR:
 			return olc::Pixel(0xffee9999);
+		case HEAVEN_STONE:
+			return olc::Pixel(0xffffdddd);
+		case HELL_STONE:
+			return olc::Pixel(0xff042233);
 		default:
 			return olc::Pixel(0xff888888);
 		};
@@ -23,6 +27,10 @@ namespace gameplay {
 			return 100;
 		case AIR:
 			return 1;
+		case HEAVEN_STONE:
+			return 0;
+		case HELL_STONE:
+			return 100;
 		default:
 			return 1;
 		}
